fce262d5b855571c6c007e61d681b2881806acb4
L.N. <santiloez6@gmail.com>
2025-08-05 14:57:34 -0300

    HINATA - BOT

--- plugins/ver.js
+++ plugins/ver.js
@@ -1,12 +1,9 @@
 let handler = async (m, { conn, args }) => {
   let url = args[0]
-  // Verifica si se proporciona una URL
   if (!url) return m.reply('❌ URL no válida.')
 
   try {
-    // Intenta enviar la imagen desde la URL proporcionada
     await conn.sendFile(m.chat, url, 'imagen.jpg', '', m)
-  // Captura cualquier error que pueda ocurrir al enviar la imagen
   } catch (e) {
     m.reply('⚠️ No se pudo enviar la imagen.')
   }
--- plugins/verimg.js
+++ plugins/verimg.js
@@ -1,16 +1,13 @@
 const handler = async (sock, m, { args }) => {
   const url = args[0];
-  // Check if the URL is valid
   if (!url || !url.startsWith('http')) {
     return sock.sendMessage(m.key.remoteJid, { text: '❌ URL no válida' }, { quoted: m });
   }
 
   try {
-    // Send the image message
     await sock.sendMessage(m.key.remoteJid, {
       image: { url },
       caption: '🖼 Imagen seleccionada de Pinterest.',
-    // Catch any errors
     }, { quoted: m });
   } catch (e) {
     console.error('🛑 Error al mostrar imagen:', e);
